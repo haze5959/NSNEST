9e00fee44c05974afeb507cc61c4247d9fc51eff
KwonOQ <haze5959@naver.com>
2018-01-03 21:06:46 +0900

    오랜만이다
    뭐 한지도 모르겠어 미안해 과거의 나

---
+++ src/app/Strings.ts
@@ -0,0 +1,9 @@
+export let Strings = {
+  APP_NAME : "NSNEST of Ancient",
+  APP_VERSION : "V0.1",
+  APP_COPYRIGHTS : "Copyright©2017 OQ All rights reserved.",
+
+
+  NODATA_IMAGE : "/../assets/testImage.jpg",
+  TEST_IMAGE : "url('https://material.angular.io/assets/img/examples/shiba1.jpg')"
+}
--- src/app/userInfo/app.userInfo.ts
+++ src/app/userInfo/app.userInfo.ts
@@ -1,14 +1,32 @@
-import { Component, Directive, ElementRef, Input, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {MatDialog, MAT_DIALOG_DATA} from '@angular/material';
+import { Strings } from '@app/Strings';
+
+import { DomSanitizer } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-userInfo',
   templateUrl: '/app.userInfo.html',
   styleUrls: ['/app.userInfo.css']
 })
-export class AppUserInfo {
-  constructor(public dialog: MatDialog) {}
-  title = "@assets/testImage.jpg";
+export class AppUserInfo implements OnInit {
+  appName = Strings.APP_NAME;
+  appVersion = Strings.APP_VERSION;
+  appCopyrights = Strings.APP_COPYRIGHTS;
+  profileImage = Strings.NODATA_IMAGE;
+  testImage = this.sanitizer.bypassSecurityTrustStyle(Strings.TEST_IMAGE);
+  constructor(public dialog: MatDialog, private sanitizer: DomSanitizer) {}
+
+  ngOnInit(){
+    //쿠키로 로그인 유지 날짜까지 계속 보관할거야
+    //todo
+    if(true){ //로그인이 유지되어 있다면
+
+    }else{  //로그인 안되어있음
+
+    }
+  }
 
   openSetUserInfoDialog() {
     const dialogRef = this.dialog.open(SetUserInfoDialog, {
