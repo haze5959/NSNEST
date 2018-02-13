c677a761f22c7740b01e2f336cc1581ba3e9420e
haze5959 <haze5959@naver.com>
2018-02-13 12:57:20 +0900

    Ff

--- src/app/newspeed/app.newspeed.ts
+++ src/app/newspeed/app.newspeed.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppNewspeed {
   title = 'newspeed';
-  onScroll () {
-      console.log('scrolled!!')
+  onScroll ($event) {
+      console.log('scrolled!!' + $event.currentScrollPosition);
   }
 }
