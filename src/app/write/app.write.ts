0c1af07f82ee39bc3b9ce794737a296e7f786e72
haze5959 <haze5959@naver.com>
2018-03-12 18:08:37 +0900

    Sfsf

--- src/app/write/app.write.ts
+++ src/app/write/app.write.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
 
@@ -9,11 +9,14 @@ import { ActivatedRoute, Params } from '@angular/router';
 })
 export class AppWrite implements OnInit {
 
+  @ViewChild('fileInputEl') fileInputEl:ElementRef;
+
   classify:number;
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private ElementRef:ElementRef) { }
 
   titleFormControl = new FormControl();
   editorContent = new FormControl();
+  quillInstance;
 
   editorModule = {
     toolbar: [
@@ -46,20 +49,23 @@ export class AppWrite implements OnInit {
    * 에디터 관련 메서드
    */
   onEditorCreated(quill) {
-    var toolbar = quill.getModule('toolbar');
-    toolbar.addHandler('image', this.imageHandler);
-  }
-
-  imageHandler(value) {
-    console.log(value);
+    this.quillInstance = quill;
+    var toolbar = this.quillInstance.getModule('toolbar');
+    toolbar.addHandler('image', () => {
+      this.fileInputEl.nativeElement.click();
+    });
   }
 
   addImageAndUploadServer($event) {
+    console.log($event.target.files[0]);
     for ( var i=0; i<$event.target.files.length; i++){
-      var reader = new FileReader();
-      reader.onload = (loadEvent: any) => {
-        // this.dataUrl.push(loadEvent.target.result);
-      }
-      reader.readAsDataURL($event.target.files[i]); //이게 뭐지? 프리뷰가 나타난다고 하던데..
+      // var reader = new FileReader();
+      // reader.onload = (loadEvent: any) => {
+      //   // this.dataUrl.push(loadEvent.target.result);
+      // }
+      // reader.readAsDataURL($event.target.files[i]); //이게 뭐지? 프리뷰가 나타난다고 하던데..
+      var range = this.quillInstance.getSelection(!this.quillInstance.hasFocus()); 
+      this.quillInstance.insertText(range, 'Hello', true);
+    }
   }
 }
